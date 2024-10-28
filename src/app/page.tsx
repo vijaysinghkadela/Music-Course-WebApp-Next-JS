166f90380bfa7a5c5b1ba3b208ef998c297d1a87
vijaysinghkadela <132544545+vijaysinghkadela@users.noreply.github.com>
2024-10-28 23:09:56 +0530

    added Herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
    
  <main className="min-h-screen bg-black/[0.96] antialiased">
-
+<HeroSection/>
  </main>
   );
 }
