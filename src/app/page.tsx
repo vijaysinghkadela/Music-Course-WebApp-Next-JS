1857d6736e4fc54d094df1bb3a64c27d9877b6b1
vijaysinghkadela <132544545+vijaysinghkadela@users.noreply.github.com>
2024-10-29 13:57:21 +0530

    added Card Hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
-
 
 export default function Home() {
   return (
-   
- <main className="min-h-screen bg-black/[0.96] antialiased">
-<HeroSection/>
-<FeaturedCourses/>
-<WhyChooseUs/>
-<MusicSchoolTestimonialCards/>
- </main>
+    <main className="min-h-screen bg-black/[0.96] antialiased">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonialCards />
+      <UpcomingWebinars />
+    </main>
   );
 }
