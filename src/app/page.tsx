8d9db61ccc3dbee75ca2a1322cfdeeb67b155d7d
vijaysinghkadela <132544545+vijaysinghkadela@users.noreply.github.com>
2024-10-29 11:40:48 +0530

    added Sticky Scroll Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
  <main className="min-h-screen bg-black/[0.96] antialiased">
 <HeroSection/>
 <FeaturedCourses/>
+<WhyChooseUs/>
  </main>
   );
 }
